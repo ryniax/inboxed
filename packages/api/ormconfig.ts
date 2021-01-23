3f0a01a43d388dfa469a9e83c512bd8c899919d7
gLenczuk <glenczuk10@gmail.com>
2021-01-23 13:24:06 +0100

    style: run linters by script

--- packages/api/ormconfig.ts
+++ packages/api/ormconfig.ts
@@ -1,18 +1,18 @@
-import { ConnectionOptions } from "typeorm";
-import { accessEnv } from "./src/utils/accessEnv";
+import { ConnectionOptions } from 'typeorm';
+import { accessEnv } from './src/utils/accessEnv';
 
 const isProduction = accessEnv('NODE_ENV') === 'production';
 
 const connectionOptions: ConnectionOptions = {
-    type: 'postgres',
-    host: accessEnv('DEV_DB_HOST'),
-    port: 5000,
-    username: accessEnv('DEV_DB_USER'),
-    password: accessEnv('DEV_DB_PASSWORD'),
-    database: accessEnv('DEV_DB_NAME'),
-    logging: !isProduction,
-    migrationsRun: true,
-    entities: ['./src/models/*.ts']
-}
+  type: 'postgres',
+  host: accessEnv('DEV_DB_HOST'),
+  port: 5000,
+  username: accessEnv('DEV_DB_USER'),
+  password: accessEnv('DEV_DB_PASSWORD'),
+  database: accessEnv('DEV_DB_NAME'),
+  logging: !isProduction,
+  migrationsRun: true,
+  entities: ['./src/models/*.ts'],
+};
 
 module.exports = connectionOptions;
