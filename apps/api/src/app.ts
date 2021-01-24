e297d9e4decf0c40fe84794248fc22b06d32f97d
gLenczuk <glenczuk10@gmail.com>
2021-01-24 10:33:15 +0100

    feat: add protection with helmet

--- apps/api/src/app.ts
+++ apps/api/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import http from 'http';
+import helmet from 'helmet';
+
 import { connectDatabase } from './database/connection';
 
 const app = express();
 connectDatabase();
+
+app.use(helmet());
 
 export const httpServer = http.createServer(app);
