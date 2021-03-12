e437ed7042f0cac169a41c9fe804ba308a551f58
Konrad Szatanek <kszatanek91@gmail.com>
2021-03-12 19:54:04 +0100

    chore: used function capitalize to improve data display

--- apps/@inboxed-web/src/components/auth-forms/RegisterForm.vue
+++ apps/@inboxed-web/src/components/auth-forms/RegisterForm.vue
@@ -1,32 +1,32 @@
 <template>
   <AuthFormContainer>
-    <AuthFormTitle>{{ $t('authForms.singUp') }}</AuthFormTitle>
+    <AuthFormTitle>{{ capitalize($t('authForms.singUp')) }}</AuthFormTitle>
     <form @submit.prevent="registerUser">
       <div class="form-inputs">
         <AuthFormInput
-          :label="$t('authForms.email')"
+          :label="capitalize($t('authForms.email'))"
           inputType="text"
           iconName="email-icon"
           @input="setInputValue($event.target.value, 'email')"
         />
         <AuthFormInput
-          :label="$t('authForms.username')"
+          :label="capitalize($t('authForms.username'))"
           inputType="text"
           iconName="user-icon"
           @input="setInputValue($event.target.value, 'username')"
         />
         <AuthFormInput
-          :label="$t('authForms.password')"
+          :label="capitalize($t('authForms.password'))"
           inputType="password"
           iconName="password-icon"
-          :underLabel="$t('authForms.forgotPassword')"
+          :underLabel="capitalize($t('authForms.forgotPassword'))"
           @input="setInputValue($event.target.value, 'password')"
         />
       </div>
       <div class="form-buttons">
-        <AuthFormButton>{{ $t('authForms.register') }}</AuthFormButton>
-        <ButtonsDivider>{{ $t('authForms.hasAccount') }}</ButtonsDivider>
-        <AuthFormButton outline>{{ $t('authForms.logIn') }}</AuthFormButton>
+        <AuthFormButton>{{ capitalize($t('authForms.register')) }}</AuthFormButton>
+        <ButtonsDivider>{{ capitalize($t('authForms.hasAccount')) }}</ButtonsDivider>
+        <AuthFormButton outline>{{ capitalize($t('authForms.logIn')) }}</AuthFormButton>
       </div>
     </form>
   </AuthFormContainer>
@@ -38,6 +38,7 @@ import AuthFormContainer from '@/components/auth-forms/containers/AuthFormContai
 import AuthFormInput from '@/components/auth-forms/core/AuthFormInput.vue';
 import AuthFormButton from '@/components/auth-forms/core/AuthFormButton.vue';
 import ButtonsDivider from '@/components/auth-forms/utils/ButtonsDivider.vue';
+import capitalize from '@/utils/capitalize';
 
 export default {
   components: {
@@ -46,6 +47,11 @@ export default {
     AuthFormInput,
     AuthFormButton,
     ButtonsDivider,
+  },
+  setup() {
+    return {
+      capitalize,
+    };
   },
 };
 </script>
