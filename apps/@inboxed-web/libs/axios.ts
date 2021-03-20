d8a15b1e169e15d39d323183b24b713d99d6f6fc
Konrad Szatanek <kszatanek91@gmail.com>
2021-03-20 10:15:49 +0100

    refactor: small changes in axios configuration.

--- apps/@inboxed-web/libs/axios.ts
+++ apps/@inboxed-web/libs/axios.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 
-const instance = axios.create({
+export default{
+axios : axios.create({
     baseUrl:'localhost:3000/api/v1'
 })
- instance.default.headers.common['Authorization'] = 'authorization';
+}
 
- export default instance;
+ 
