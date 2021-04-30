a305cf403b6bf11a6dadda9f31808f840bfd0981
gLenczuk <lenczuk.dev@gmail.com>
2021-04-30 23:14:50 +0200

    feat: prepare initial channel view with message input

--- apps/@inboxed-web/src/components/CurrentChannel.vue
+++ apps/@inboxed-web/src/components/CurrentChannel.vue
@@ -1,6 +1,16 @@
 <template>
-  <div class="currentChannel">
-    {{ currentChannel }}
+  <div class="current-channel">
+    <div class="current-channel__messages">
+      messages
+    </div>
+    <div class="current-channel__input">
+      <textarea
+        placeholder="Type your message here..."
+        @keydown.prevent.enter="sendMessage"
+        rows="6"
+        :resize="false"
+      />
+    </div>
   </div>
 </template>
 
@@ -14,17 +24,45 @@ export default defineComponent({
     const ServersModule = getModule(Servers);
 
     const currentChannel = computed(() => ServersModule.getCurrentChannel);
+    const sendMessage = () => console.log('sent');
 
     return {
       currentChannel,
+      sendMessage,
     };
   },
 });
 </script>
 
 <style scoped lang="scss">
-.currentChannel {
+.current-channel {
   width: 80%;
   color: white;
+  padding: 12px;
+  display: flex;
+  flex-direction: column;
+
+  &__messages {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-end;
+    flex-grow: 1;
+    padding: 20px 5px;
+    width: 100%;
+  }
+
+  &__input {
+    width: 100%;
+
+    textarea {
+      width: 100%;
+      resize: none;
+      padding: 14px;
+      border-radius: 10px;
+      color: white;
+      background-color: $primary-color;
+      outline: none;
+    }
+  }
 }
 </style>
